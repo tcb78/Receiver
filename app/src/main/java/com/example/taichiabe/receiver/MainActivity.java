b39e1c5bd3fb6048c7c5b117e34d04b387641376
Taichi Abe <ma18004@shibaura-it.ac.jp>
2020-03-13 22:22:12 +0900

    リファクタリング

--- app/src/main/java/com/example/taichiabe/receiver/MainActivity.java
+++ app/src/main/java/com/example/taichiabe/receiver/MainActivity.java
@@ -4,7 +4,6 @@ package com.example.taichiabe.receiver;
  *==========================================================*/
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.Locale;
 
 import android.app.Activity;
 import android.media.AudioFormat;
@@ -33,8 +32,6 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
     boolean isRecording = false;
     Thread fft;
     private TimeMeasure tm;
-    SdLog sdlog;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,8 +39,8 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
         TextView receivingFreqText = findViewById(R.id.receivingFreqText);
         receivingFreqText.setText(R.string.receivingFreqText);
-        TextView decibelText = findViewById(R.id.decibelText);
-        decibelText.setText(R.string.decibelText);
+        TextView thresholdText = findViewById(R.id.thresholdText);
+        thresholdText.setText(R.string.thresholdText);
         Switch receivingSwitch = findViewById(R.id.receivingSwitch);
         receivingSwitch.setOnCheckedChangeListener(this);
     }
@@ -55,28 +52,12 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
         if(isChecked) {
-
             EditText receivingFreqEdit = findViewById(R.id.receivingFreqEdit);
-            EditText decibelEdit = findViewById(R.id.decibelEdit);
+            EditText thresholdEdit = findViewById(R.id.thresholdEdit);
 
             final int RECEIVING_FREQ = Integer.parseInt(receivingFreqEdit.getText().toString());
-            final int DECIBEL = Integer.parseInt(decibelEdit.getText().toString());
-
-            final String FILENAME = String.valueOf(System.currentTimeMillis());
-
-            /*
-            if(RECVFREQ == 18000)      FFTPOINT = 3344;
-            else if(RECVFREQ == 18500) FFTPOINT = 3436;
-            else if(RECVFREQ == 19000) FFTPOINT = 3530;
-            else if(RECVFREQ == 19500) FFTPOINT = 3622;
-            else if(RECVFREQ == 20000) FFTPOINT = 3716;
-            else if(RECVFREQ == 20500) FFTPOINT = 3808;
-            else if(RECVFREQ == 21000) FFTPOINT = 3900;
-            else if(RECVFREQ == 21500) FFTPOINT = 3994;
-            else if(RECVFREQ == 22000) FFTPOINT = 4086;
-            */
+            final double THRESHOLD = Integer.parseInt(thresholdEdit.getText().toString());
 
             //実験用デバイスではminBufSize = 3584
             final int MIN_BUFFER_SIZE = AudioRecord.getMinBufferSize(
@@ -110,7 +91,6 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
                     byte[] recordData = new byte[RECORD_BUFFER_SIZE];
                     while (isRecording) {
-
                         //計測開始
                         tm.startMeasure();
 
@@ -122,14 +102,11 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
                         //パワースペクトル・デシベルの計算
                         double[] decibelFrequencySpectrum = computePowerSpectrum(fftData);
                         //ドップラー効果を考慮した接近検知
-                        detectApproaching(RECEIVING_FREQ, DECIBEL, decibelFrequencySpectrum, FILENAME);
-
+                        detectApproaching(RECEIVING_FREQ, THRESHOLD, decibelFrequencySpectrum);
                         //計測終了
                         tm.finishMeasure();
-                        sdlog.put("time1-" + FILENAME, "process time:" + tm.measureTimeSec().substring(0, 5));
                         //処理時間出力
                         tm.printTimeSec();
-
                     }
                     audioRec.stop();
                     audioRec.release();
@@ -140,7 +117,6 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
             fft.start();
 
         } else {
-
             if(audioRec.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
                 vib.cancel();
                 audioRec.stop();
@@ -153,7 +129,6 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
     @Override
     public void onPause() {
         super.onPause();
-
         if(audioRec.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
             audioRec.stop();
             isRecording = false;
@@ -163,7 +138,6 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
     @Override
     public void onDestroy() {
         super.onDestroy();
-
         if(audioRec.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
             audioRec.stop();
             audioRec.release();
@@ -173,12 +147,11 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
     /**
      * エンディアン変換
-     * @param   buf     受信バッファデータ
-     * @param   bufferSize  受信バッファサイズ
-     * @return  shortData   エンディアン変換後short型データ
+     * @param buf 受信バッファデータ
+     * @param bufferSize 受信バッファサイズ
+     * @return エンディアン変換後short型データ
      */
     public short[] toLittleEndian(byte[] buf, int bufferSize) {
-
         //エンディアン変換
         //配列bufをもとにByteBufferオブジェクトbfを作成
         ByteBuffer bf = ByteBuffer.wrap(buf);
@@ -188,11 +161,9 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
         bf.order(ByteOrder.LITTLE_ENDIAN);
         short[] shortData = new short[bufferSize / 2];
 
-        int bfFirst = bf.position();
-        int bfLast = bf.capacity() / 2;
-        //位置から容量まで
-        //FIXME:変数
-        for (int i = bfFirst; i < bfLast; i++) {
+        int bfBegin = bf.position();
+        int bfEnd = bf.capacity() / 2;
+        for (int i = bfBegin; i < bfEnd; i++) {
             //short値を読むための相対getメソッド
             //現在位置の2バイトを読み出す
             shortData[i] = bf.getShort();
@@ -202,11 +173,10 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
     /**
      * 高速フーリエ変換
-     * @param   shortData   エンディアン変換後データ
-     * @return  fftData     フーリエ変換後データ
+     * @param shortData エンディアン変換後データ
+     * @return フーリエ変換後データ
      */
     public double[] fastFourierTransform(short[] shortData) {
-
         //FFTクラスの作成と値の引き渡し
         FFT4g fft = new FFT4g(FFT_SIZE);
         double[] fftData = new double[FFT_SIZE];
@@ -220,17 +190,15 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
     /**
      * パワースペクトル・デシベルの計算
-     * @param   fftData         フーリエ変換後のデータ
-     * @return  decibelFrequencySpectrum    デシベル値
+     * @param fftData フーリエ変換後のデータ
+     * @return デシベル値
      */
     public double[] computePowerSpectrum(double[] fftData) {
-
         //パワースペクトル・デシベルの計算
         double[] powerSpectrum = new double[FFT_SIZE / 2];
         //DeciBel Frequency Spectrum
         double[] decibelFrequencySpectrum = new double[FFT_SIZE / 2];
         for(int i = 0; i < FFT_SIZE; i += 2) {
-            //dbfs[i / 2] = (int) (20 * Math.log10(Math.sqrt(Math.pow(FFTdata[i], 2) + Math.pow(FFTdata[i + 1], 2)) / dB_baseline));
             powerSpectrum[i / 2] = Math.sqrt(Math.pow(fftData[i], 2) + Math.pow(fftData[i + 1], 2));
             decibelFrequencySpectrum[i / 2] = (int) (20 * Math.log10(powerSpectrum[i / 2] / DB_BASELINE));
         }
@@ -239,23 +207,17 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
     /**
      * 接近検知アルゴリズム
-     * @param freq          受信周波数
-     * @param decibel       検知デシベル値
-     * @param decibelFrequencySpectrum  デシベル値周波数成分
-     * @param filename      ファイル名
+     * @param freq 受信周波数
+     * @param decibel 検知デシベル値
+     * @param decibelFrequencySpectrum デシベル値周波数成分
      */
-    public void detectApproaching(int freq, double decibel, double[] decibelFrequencySpectrum, String filename) {
-
+    public void detectApproaching(int freq, double decibel, double[] decibelFrequencySpectrum) {
         //ドップラー効果考慮 Doppler Effect
         //500Hzの幅で接近検知
-        int deFirstFrame = frameSetting(freq) / 2;
-        int deLastFrame = frameSetting(freq + 500) / 2;
-        //FIXME:/2
-        for(int j = deFirstFrame; j < deLastFrame; j++) {
+        int detectFrameBegin = frameSetting(freq);
+        int detectFrameEnd = frameSetting(freq + 500);
+        for(int j = detectFrameBegin; j < detectFrameEnd; j++) {
             if(decibelFrequencySpectrum[j] > decibel) {
-                //検知周波数
-                sdlog.put("freq1-" + filename, String.format(Locale.US, "%.3f", j * RESOLUTION) + " : " + String.valueOf(decibelFrequencySpectrum[j]));
-                //インスタンス取得が外だから振動しない可能性あり
                 vib.cancel();
                 vib.vibrate(200);
                 break;
@@ -265,15 +227,14 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
     /**
      * 周波数からフレーム設定
-     * @param   freq    周波数
-     * @return  frame   フレーム
+     * @param freq 周波数
+     * @return フレーム
      */
     public int frameSetting(int freq) {
-
         int frame = (int)(2 * freq / RESOLUTION);
         if(frame % 2 == 1) {
             frame++;
         }
-        return frame;
+        return frame / 2;
     }
 }
